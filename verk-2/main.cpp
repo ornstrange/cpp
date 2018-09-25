fa06b51b4e8731dd2d8dbad2cd7c16eb5cc2bcd9
ornstrange <ornst278@nemi.tskoli.is>
2018-09-25 17:26:50 +0000

    jæja

--- verk-2/main.cpp
+++ verk-2/main.cpp
@@ -3,11 +3,79 @@
 
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 
+typedef unsigned int uint;
+
+int tempIp[4];
+
+string dottedIp(int *ip) {
+    string out;
+    for (int i = 0; i < 3; i++)
+        out += to_string(ip[i]) + ".";
+    out += ip[3];
+    return out;
+};
+
+string ipPadded(int *ip) {
+    string out;
+    int strLen;
+    out += dottedIp(ip);
+    strLen = out.length();
+    for (int i = 0; i < 16 - strLen; i++)
+        out += " ";
+    out += "|";
+    return out;
+};
+
+string maskPadded(uint mask) {
+    string out = "255.255.255.";
+    int strLen;
+    out += to_string(mask);
+    strLen = out.length();
+    for (int i = 0; i < 16 - strLen; i++)
+        out += " ";
+    out += "|";
+    return out;
+};
+
+int * firstUsable(int *ip) {
+    copy(ip, ip+4, tempIp);
+    tempIp[3] += 1;
+    return tempIp;
+};
+
+int * lastUsable(int *ip, int magicNum) {
+    copy(ip, ip+4, tempIp);
+    tempIp[3] += magicNum - 2;
+    return tempIp;
+};
+
+int * broadCast(int *ip, int magicNum) {
+    copy(ip, ip+4, tempIp);
+    tempIp[3] += magicNum - 1;
+    return tempIp;
+};
+
+void printIp(int *ip, uint mask, int magicNum) {
+    cout << ipPadded(ip)
+         << ipPadded(firstUsable(ip))
+         << ipPadded(lastUsable(ip, magicNum))
+         << ipPadded(broadCast(ip, magicNum))
+         << maskPadded(mask)
+         << endl;
+};
+
+int * setIpNetwork(int *ip, int network) {
+    copy(ip, ip+4, tempIp);
+    tempIp[3] = network;
+    return tempIp;
+};
+
 int main() {
-    unsigned int ip[4];
-    int bits, div;
+    int ip[4], bits, div, bitsReq, lastOne, magicNumber;
+    uint netMask;
     string dashLine = "----------------+";
 
     cout << "Sláðu inn upphafsnet: ";
@@ -26,7 +94,14 @@ int main() {
          << "Maski           |"  << endl;
     cout << dashLine << dashLine << dashLine << dashLine << dashLine << endl;
 
-    
+    bitsReq = log2(div);
+    lastOne = 32 - (bits + bitsReq);
+    magicNumber = pow(2, lastOne);
+    netMask = 0xffffffff << (32 - (bits + bitsReq));
+
+    for (int i = 0; i < div; i++) {
+        printIp(setIpNetwork(ip, i * magicNumber), netMask, magicNumber);
+    };
 
     return 0;
-}
+};

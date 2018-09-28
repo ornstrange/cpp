471b8f12949a227c3dee1a17590002d3899ff49e
ornstrange <ornst278@nemi.tskoli.is>
2018-09-28 13:31:33 +0000

    fuck

--- verk-2/main2.cpp
+++ verk-2/main2.cpp
@@ -10,21 +10,17 @@ typedef unsigned int uint; // nenni ekki að skrifa alltaf unsigned int
 #define rep(i,a,b) for(int i = (a); i < (b); i++) // styttri for loop
 
 void uitoa(uint num, uint *arr) { // unsigned int í array
-    rep(i,0,4) {
-        arr[i] = num << i*8 >> 24;
-    }
+    rep(i,0,4) arr[i] = num << i*8 >> 24;
 }
 
 uint atoui(uint *arr) {
     uint output;
-    output = arr[0] << 24 | arr[1] << 16 |
-             arr[2] << 8 | arr[3];
+    output = arr[0] << 24 | arr[1] << 16 | arr[2] << 8 | arr[3];
     return output;
 }
 
 void addToIp(uint *arr, uint x = 1) {
-    uint temp = atoui(arr);
-    temp += x;
+    uint temp = atoui(arr) + x;
     uitoa(temp, arr);
 }
 
@@ -41,13 +37,13 @@ string printIp(uint *ip) { // prenta ip fallega
 
 int main() {
     uint ip[4], netMask, netMaskArr[4], bits, div, bitsReq, offset,
-         lastOne, magicNumber, ones = 0xffffffff;
+         lastOne, magicNumber, ones = 0xffffffff, addAlg[4];
     string dashLine = "----------------+";
 
     cout << "Sláðu inn upphafsnet: ";
     rep(i,0,4) cin >> ip[i];
     offset = ip[3];
- 
+
     cout << "Sláðu inn fjölda netbita í upphafsneti: ";
     cin >> bits;
 
@@ -64,16 +60,13 @@ int main() {
     netMask = ones << 32 - (bits + (int)log2(div));
     uitoa(netMask, netMaskArr);
     magicNumber = pow(2, 32 - (bits + (int)log2(div)));
+    addAlg = {1, magicNumber - 3, 1, 1};
 
     rep(i,0,div) {
-        cout << printIp(ip);
-        addToIp(ip);
-        cout << printIp(ip);
-        addToIp(ip, magicNumber - 3);
-        cout << printIp(ip);
-        addToIp(ip);
-        cout << printIp(ip);
-        addToIp(ip);
+        rep(j, 0, 4) {
+            cout << printIp(ip);
+            addToIp(ip, addAlg[j]);
+        }
         cout << printIp(netMaskArr) << endl;
     }
     return 0;
